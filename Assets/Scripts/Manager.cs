c3935591d7adb16e8cee0c36e0d37244e47ab317
Zak <zadj@create.aau.dk>
2021-07-23 09:01:13 +0200

    LSL marker fixed
    EEGLAB now finds the markers

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -62,6 +62,7 @@ public class Manager : MonoBehaviour
     {
         // Getting LSL stream
         marker = FindObjectOfType<LSLMarkerStream>();
+        marker.GetComponent<LSLMarkerStream>().lslStreamType = "Markers";
 
         // Find gameobjects
         XRRig = GameObject.Find("[CameraRig]");
